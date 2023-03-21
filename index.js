2dadf065b5763cb3cb295e216eccac40eb3a5165
Manimozhi Neethimohan <Manimozhin@gain-insights.com>
2023-03-21 18:20:53 +0530

    modals and tabs

--- index.js
+++ index.js
@@ -21,3 +21,37 @@ window.onresize=window.onload=()=>{
         list.style.display="none";
     }
 }
+
+//Tabs
+const li=document.querySelectorAll("#list a");  //list array
+
+const main_content=document.querySelectorAll("#content .main");  //main array
+
+
+function removeActive(){
+    li.forEach((l)=>{
+      l.classList.remove("active");
+    });
+    main_content.forEach((m)=>{
+      m.classList.remove("active");
+    });
+}
+
+li.forEach((l,i)=>{
+  l.addEventListener('click',()=>{
+    removeActive();
+    l.classList.add("active");
+    main_content[i].classList.add("active");
+    
+  });
+});
+
+//modals
+const hoverbtn=document.querySelectorAll('.hoverbtn');
+console.log(hoverbtn);
+function displayModal(){
+  alert("displayed");
+}
+hoverbtn.forEach((hb)=>{
+  hb.addEventListener('click',displayModal);
+});
